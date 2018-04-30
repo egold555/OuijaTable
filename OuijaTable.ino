f93d122f5f9540b0fb6fc890dece7f50e3801e68
Eric Golde <eric@golde.org>
2018-04-29 19:50:57 -0700

    Added pullup switches and a reset

--- OuijaTable.ino
+++ OuijaTable.ino
@@ -3,10 +3,14 @@
 #define CHAR_TABLE_LEGTH 39
 
 const int LIGHT_PIN = 10;
+const int LIMIT_LR = A0;
+const int LIMIT_UD = A1;
 
 const int SPEED = 100;
-const int X_AMOUNT = 1500;
-const int Y_AMOUNT = 500;
+const int MIN_X = 100;
+const int MIN_Y = 0;
+const int MAX_X = 1500;
+const int MAX_Y = 550;
 
 //planchet's current position
 int posX = 0;
@@ -62,15 +66,43 @@ unsigned int charTable[CHAR_TABLE_LEGTH][3] = {
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello World!");
+  Serial.println("Initalising...");
+  
   pinMode(LIGHT_PIN, OUTPUT);
   digitalWrite(LIGHT_PIN, HIGH);
+
+  pinMode(LIMIT_LR, INPUT_PULLUP);
+  pinMode(LIMIT_UD, INPUT_PULLUP);
+  
   motorLR.setSpeed(SPEED);
   motorUD.setSpeed(SPEED);
+
+  Serial.println("Resetting...");
+  delay(100);
+  reset();
+  delay(100);
+  Serial.println("Welcome!");
 }
 
 void loop() {
   serialCalbrate();
+}
+
+void reset(){
+  //Move until we hit the limit switches
+  while(digitalRead(LIMIT_LR) != LOW){
+    motorLR.step(1, FORWARD, DOUBLE);
+  }
+  motorLR.release();
+
+  while(digitalRead(LIMIT_UD) != LOW){
+    motorUD.step(1, BACKWARD, DOUBLE);
+  }
+  motorUD.release();
+
+  posX = MAX_X;
+  posY = 0;
+  
 }
 
 void lightFlicker() {
@@ -87,25 +119,57 @@ void lightFlicker() {
 }
 
 void seekTest() {
-  motorLR.step(X_AMOUNT, FORWARD, DOUBLE);
+  motorLR.step(MAX_X, FORWARD, DOUBLE);
   motorLR.release();
-  motorUD.step(Y_AMOUNT, FORWARD, DOUBLE);
+  motorUD.step(MAX_Y, FORWARD, DOUBLE);
   motorUD.release();
   delay(50);
-  motorLR.step(X_AMOUNT, BACKWARD, DOUBLE);
+  motorLR.step(MAX_X, BACKWARD, DOUBLE);
   motorLR.release();
-  motorUD.step(Y_AMOUNT, BACKWARD, DOUBLE);
+  motorUD.step(MAX_Y, BACKWARD, DOUBLE);
   motorUD.release();
   delay(50);
 }
 
+const int MOVE_AMOUNT = 20;
 void serialCalbrate() {
   if (Serial.available() > 0) {
-    int incomingByte = 0;
-    incomingByte = Serial.read();
+    char in = (char)Serial.read();
 
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    if (in == 'w') {
+      move(posX, posY + 20);
+    }
+    else if (in == 'W') {
+      move(posX, posY + 50);
+    }
+    
+    else if (in == 's') {
+      move(posX, posY - 20);
+    }
+    else if (in == 'S') {
+      move(posX, posY - 50);
+    }
+    
+    else if (in == 'a') {
+      move(posX - 20, posY);
+    }
+    else if (in == 'A') {
+      move(posX - 50, posY);
+    }
+    
+    else if (in == 'd') {
+      move(posX + 20, posY);
+    }
+    else if (in == 'D') {
+      move(posX + 50, posY);
+    }
+
+
+    Serial.print("X: ");
+    Serial.print(posX, DEC);
+    Serial.print(" Y: ");
+    Serial.print(posY, DEC);
+    Serial.println("");
   }
 }
 
@@ -125,3 +189,47 @@ void moveToSymbol(char character)
   int toX = charTable[tableIndex][0];
   int toY = charTable[tableIndex][1];
 }
+
+//void movePlus(int tx, int ty){
+//  move(tx + posX, ty + posY);
+//}
+
+void move(int tx, int ty) { //Async sometime
+
+  int dirX, dirY;
+  int moveX, moveY;
+
+  if(tx < MIN_X){tx = MIN_X;}
+  if(ty < MIN_Y){ty = MIN_Y;}
+
+  if(tx > MAX_X){tx = MAX_X;}
+  if(ty > MAX_Y){ty = MAX_Y;}
+
+  if (tx < posX) {
+    dirX = BACKWARD;
+    moveX = posX - tx;
+  }
+  else {
+    dirX = FORWARD;
+    moveX = tx - posX;
+  }
+
+  if (ty < posY) {
+    dirY = BACKWARD;
+    moveY = posY - ty;
+  }
+  else {
+    dirY = FORWARD;
+    moveY = ty - posY;
+  }
+
+  motorLR.step(moveX, dirX, DOUBLE);
+  motorLR.release();
+  
+  motorUD.step(moveY, dirY, DOUBLE);
+  motorUD.release();
+  
+  posX = tx;
+  posY = ty;
+}
+
