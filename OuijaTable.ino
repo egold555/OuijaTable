55f482f43cd6fb470968553f198234b00f922cea
Eric Golde <eric@golde.org>
2018-05-01 11:18:01 -0700

    added line seeker test removed Stringed Serial

--- OuijaTable.ino
+++ OuijaTable.ino
@@ -22,7 +22,7 @@ long timeNextOff = 0;
 AF_Stepper motorLR(200, 1);
 AF_Stepper motorUD(200, 2);
 
-unsigned int charTable[CHAR_TABLE_LEGTH][3] = {
+int charTable[CHAR_TABLE_LEGTH][3] = {
   -1, -1, 'a',
   -1, -1, 'b',
   -1, -1, 'c',
@@ -82,10 +82,12 @@ void setup() {
   reset();
   delay(100);
   Serial.println("[INFO] Welcome!");
+  delay(100);
+  
 }
 
 void loop() {
-  serialCalbrate();
+  //serialCalbrate();
 }
 
 void reset() {
@@ -117,6 +119,86 @@ void lightFlicker() {
     timeNextOn = currentTime + random(40, 500); //off for
   }
 }
+
+int findSymbolInTable(char symbol)
+{
+  for (int tableIndex = 0; tableIndex <= CHAR_TABLE_LEGTH; tableIndex++) {
+    if (symbol == char(charTable[tableIndex][2])) {
+      return tableIndex;
+    }
+  }
+  return -2;
+}
+
+void moveToSymbol(char character)
+{
+  int tableIndex = findSymbolInTable(character);
+  int toX = charTable[tableIndex][0];
+  int toY = charTable[tableIndex][1];
+
+  if (toX == -1 || toY == -1) {
+    //Serial.println(String("[ERROR] Character ") + String(character) + String(" does not have a valid XY coords! (X: ") + String(toX) + String(" Y: ") + String(toY));
+  }
+
+  if (toX == -2 || toY == -2) {
+    //Serial.println(String("[ERROR] Character ") + String(character) + String(" does not exist in the character list!"));
+  }
+  move(toX, toY);
+}
+
+
+void move(int tx, int ty) { //Async sometime
+
+  int dirX, dirY;
+  int moveX, moveY;
+
+  if (tx < MIN_X) {
+    tx = MIN_X;
+  }
+  if (ty < MIN_Y) {
+    ty = MIN_Y;
+  }
+
+  if (tx > MAX_X) {
+    tx = MAX_X;
+  }
+  if (ty > MAX_Y) {
+    ty = MAX_Y;
+  }
+
+  if (tx < posX) {
+    dirX = BACKWARD;
+    moveX = posX - tx;
+  }
+  else {
+    dirX = FORWARD;
+    moveX = tx - posX;
+  }
+
+  if (ty < posY) {
+    dirY = BACKWARD;
+    moveY = posY - ty;
+  }
+  else {
+    dirY = FORWARD;
+    moveY = ty - posY;
+  }
+
+  motorLR.step(moveX, dirX, DOUBLE);
+
+  motorUD.step(moveY, dirY, DOUBLE);
+
+  posX = tx;
+  posY = ty;
+}
+
+void release()
+{
+  motorLR.release();
+  motorUD.release();
+}
+
+//===================== Test Code ====================
 
 void seekTest() {
   motorLR.step(MAX_X, FORWARD, DOUBLE);
@@ -164,6 +246,7 @@ void serialCalbrate() {
       move(posX + 50, posY);
     }
 
+    release();
 
     Serial.print("[DEBUG] X: ");
     Serial.print(posX, DEC);
@@ -173,76 +256,23 @@ void serialCalbrate() {
   }
 }
 
-int findSymbolInTable(char symbol)
-{
-  for (int tableIndex = 0; tableIndex <= CHAR_TABLE_LEGTH; tableIndex++) {
-    if (symbol == char(charTable[tableIndex][2])) {
-      return tableIndex;
-    }
+// https://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
+void line(int x0, int y0, int x1, int y1) {
+ 
+  int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+  int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
+  int err = (dx>dy ? dx : -dy)/2, e2;
+ 
+  while(true) {
+    //move(x0,y0);
+    Serial.print("M("); Serial.print(x0); Serial.print(","); Serial.print(y0); Serial.println(")");
+    if (x0==x1 && y0==y1) 
+      break;
+    e2 = err;
+    if (e2 >-dx) { err -= dy; x0 += sx; }
+    if (e2 < dy) { err += dx; y0 += sy; }
   }
-  return -2;
 }
 
-void moveToSymbol(char character)
-{
-  int tableIndex = findSymbolInTable(character);
-  int toX = charTable[tableIndex][0];
-  int toY = charTable[tableIndex][1];
 
-  if(toX == -1 || toY == -1){
-    Serial.println(String("[ERROR] Character ") + String(character) + String(" does not have a valid XY coords! (X: ") + String(toX) + String(" Y: ") + String(toY));
-  }
-
-  if(toX == -2 || toY == -2){
-    Serial.println(String("[ERROR] Character ") + String(character) + String(" does not exist in the character list!"));
-  }
-  move(toX, toY);
-}
-
-void move(int tx, int ty) { //Async sometime
-
-  int dirX, dirY;
-  int moveX, moveY;
-
-  if (tx < MIN_X) {
-    tx = MIN_X;
-  }
-  if (ty < MIN_Y) {
-    ty = MIN_Y;
-  }
-
-  if (tx > MAX_X) {
-    tx = MAX_X;
-  }
-  if (ty > MAX_Y) {
-    ty = MAX_Y;
-  }
-
-  if (tx < posX) {
-    dirX = BACKWARD;
-    moveX = posX - tx;
-  }
-  else {
-    dirX = FORWARD;
-    moveX = tx - posX;
-  }
-
-  if (ty < posY) {
-    dirY = BACKWARD;
-    moveY = posY - ty;
-  }
-  else {
-    dirY = FORWARD;
-    moveY = ty - posY;
-  }
-
-  motorLR.step(moveX, dirX, DOUBLE);
-  motorLR.release();
-
-  motorUD.step(moveY, dirY, DOUBLE);
-  motorUD.release();
-
-  posX = tx;
-  posY = ty;
-}
 
